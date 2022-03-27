8d23b8d8e26be84f4af444f1bf294329d3d1b89e
Suhel Makkad <MAKADSUHEL11@GMAIL.COM>
2022-03-27 19:03:29 +0530

    added series page

--- pages/series/index.js
+++ pages/series/index.js
@@ -1,19 +1,131 @@
+import { useState, useEffect } from "react";
+
 import PageHead from "../../components/PageHead";
 import TopBar from "../../components/TopBar";
 import BannerImage from "../../components/BannerImage";
 import NavBar from "../../components/NavBar";
+import Card, { LoadingCard } from "../../components/Card";
+import CardLoadingInditactor from "../../components/CardLoadingInditactor";
+import NoMoreToShow from "../../components/NoMoreToShow";
 import Footer from "../../components/Footer";
 
-export default function TvShows() {
+import axios from "axios";
+
+import useElementOnScreen from "../../hooks/useElementOnScreen";
+
+import { to, getMonthName, filterDuplicates } from "../../utils";
+
+// import styles from "../../styles/Series.module.css";
+
+export default function Series() {
+  const [bannerSeries, setBannerSeries] = useState({});
+  const [series, setSeries] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [showMoreSeries, setShowMoreSeries] = useState(true);
+  const [containerRef, isBottomVisible] = useElementOnScreen({
+    root: null,
+    rootMargin: "0px 0px 0px 0px",
+    threshold: 1,
+  });
+
+  const loadingCardCount = 20;
+  const bannerSeriesId = [
+    29695, 27932, 32777, 30534, 417, 28177, 29493, 13830, 31982, 16449, 14246, 31903,
+  ];
+
+  const getPlaceHoldersCards = () => {
+    const placeHolderCards = [];
+
+    for (let i = 0; i < loadingCardCount; i++) {
+      placeHolderCards.push(<LoadingCard key={i} />);
+    }
+
+    return placeHolderCards;
+  };
+
+  const load20Series = async () => {
+    const getSeriesURL = `/api/getSeries?offset=${offset}`;
+    const [response, error] = await to(axios.get(getSeriesURL));
+
+    if (error) {
+      return;
+    }
+
+    const data = response.data;
+
+    if (offset === response.data.total) {
+      setShowMoreSeries(false);
+      return;
+    }
+
+    setOffset((prev) => prev + data.count);
+    setSeries((prev) => filterDuplicates([...prev, ...data.results], "id"));
+  };
+
+  useEffect(async () => {
+    const randomSeriesId = bannerSeriesId[Math.floor(Math.random() * bannerSeriesId.length)];
+
+    const getSeriesURL = `/api/getSeries/${randomSeriesId}`;
+    const [response, error] = await to(axios.get(getSeriesURL));
+    if (error) {
+      return;
+    }
+
+    const bannerSeries = response.data.results[0];
+
+    setBannerSeries(bannerSeries);
+    load20Series();
+  }, []);
+
+  useEffect(() => {
+    if (!isBottomVisible || !series.length) return;
+    load20Series();
+  }, [isBottomVisible]);
+
   return (
     <div>
-      <PageHead title="Tv Shows" />
+      <PageHead title="Series" />
 
       <TopBar />
 
-      <BannerImage imageSrc={"/index/thor-bg.png"} />
+      <BannerImage
+        title={bannerSeries.title}
+        description={bannerSeries.description}
+        imageSrc={
+          bannerSeries.thumbnail &&
+          `${bannerSeries.thumbnail.path}/detail.${bannerSeries.thumbnail.extension}`
+        }
+      />
 
       <NavBar />
+
+      <main className="mainWrapper">
+        <div className="cardWrapper">
+          {series?.length
+            ? series.map((series) => (
+                <Card
+                  key={series.id}
+                  title={series.title}
+                  description={`${series.startYear} -  ${series.endYear}`}
+                  href={`/series/${series.id}`}
+                  showIfImageAvaialbe={true}
+                  src={
+                    series.thumbnail &&
+                    `${series.thumbnail.path}/portrait_uncanny.${series.thumbnail.extension}`
+                  }
+                />
+              ))
+            : getPlaceHoldersCards()}
+        </div>
+      </main>
+
+      {showMoreSeries ? (
+        <div ref={containerRef}>
+          <CardLoadingInditactor />
+        </div>
+      ) : (
+        <NoMoreToShow />
+      )}
 
       <Footer />
     </div>
